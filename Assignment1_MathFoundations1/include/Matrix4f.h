d08e15a56ce983eb21728f498b7bb78dfb3580c9
selkheir <elkheir.s@husky.neu.edu>
2020-02-01 15:39:45 -0500

    [A2] Completed implementation

--- Assignment1_MathFoundations1/include/Matrix4f.h
+++ Assignment1_MathFoundations1/include/Matrix4f.h
@@ -10,103 +10,119 @@
 #include "Vector4f.h"
 
 // Matrix 4f represents 4x4 matrices in Math
-struct Matrix4f{
+struct Matrix4f {
 private:
-    float n[4][4];  // Store each value of the matrix
+  float n[4][4];  // Store each value of the matrix
 
 public:
-    Matrix4f() = default;
+  Matrix4f() = default;
 
-    // TODO: Row or column major order you decide!
-    // Matrix constructor with 9 scalar values.
-    Matrix4f( float n00, float n01, float n02, float n03,
-              float n10, float n11, float n12, float n13,
-              float n20, float n21, float n22, float n23,
-              float n30, float n31, float n32, float n33){
+  // Matrix constructor with 9 scalar values.
+  Matrix4f(
+    float n00, float n01, float n02, float n03,
+    float n10, float n11, float n12, float n13,
+    float n20, float n21, float n22, float n23,
+    float n30, float n31, float n32, float n33) {
 
-        n[0][0] = n00; n[0][1] = n10; n[0][2] = n20; n[0][3] = n30;
-        n[1][0] = n01; n[1][1] = n11; n[1][2] = n21; n[1][3] = n31;
-        n[2][0] = n02; n[2][1] = n12; n[2][2] = n22; n[2][3] = n32;
-        n[3][0] = n03; n[3][1] = n13; n[3][2] = n23; n[3][3] = n33;
-    }
+    n[0][0] = n00; n[0][1] = n10; n[0][2] = n20; n[0][3] = n30;
+    n[1][0] = n01; n[1][1] = n11; n[1][2] = n21; n[1][3] = n31;
+    n[2][0] = n02; n[2][1] = n12; n[2][2] = n22; n[2][3] = n32;
+    n[3][0] = n03; n[3][1] = n13; n[3][2] = n23; n[3][3] = n33;
+  }
 
-    // Matrix constructor from four vectors.
-    // Note: 'd' will almost always be 0,0,0,1
-    Matrix4f(const Vector4f& a, const Vector4f& b, const Vector4f& c, const Vector4f& d){
-      n[0][0] = a.x; n[0][1] = b.x; n[0][2] = c.x; n[0][3] = d.x;
-      n[1][0] = a.y; n[1][1] = b.y; n[1][2] = c.y; n[1][3] = d.y;
-      n[2][0] = a.z; n[2][1] = b.z; n[2][2] = c.z; n[2][3] = d.z;
-      n[3][0] = a.w; n[3][1] = b.w; n[3][2] = c.w; n[3][3] = d.w;
-    }
+  // Matrix constructor from four vectors.
+  // Note: 'd' will almost always be 0,0,0,1
+  Matrix4f(const Vector4f &a, const Vector4f &b, const Vector4f &c, const Vector4f &d) {
+    n[0][0] = a.x; n[0][1] = b.x; n[0][2] = c.x; n[0][3] = d.x;
+    n[1][0] = a.y; n[1][1] = b.y; n[1][2] = c.y; n[1][3] = d.y;
+    n[2][0] = a.z; n[2][1] = b.z; n[2][2] = c.z; n[2][3] = d.z;
+    n[3][0] = a.w; n[3][1] = b.w; n[3][2] = c.w; n[3][3] = d.w;
+  }
 
-    // Makes the matrix an identity matrix
-    void identity(){
-        // TODO:
-    }
+  // Makes the matrix an identity matrix
+  void identity() {
+    n[0][0] = 1; n[0][1] = 0; n[0][2] = 0; n[0][3] = 0;
+    n[1][0] = 0; n[1][1] = 1; n[1][2] = 0; n[1][3] = 0;
+    n[2][0] = 0; n[2][1] = 0; n[2][2] = 1; n[2][3] = 0;
+    n[3][0] = 0; n[3][1] = 0; n[3][2] = 0; n[3][3] = 1;
+  }
 
-    // Index operator with two dimensions
-    // Example: M(1,1) returns row 1 and column 1 of matrix M.
-    float& operator ()(int i, int j){
-      return (n[j][i]);
-    }
+  // Index operator with two dimensions
+  // Example: M(1,1) returns row 1 and column 1 of matrix M.
+  float &operator ()(int i, int j) {
+    return (n[j][i]);
+  }
 
-    // Index operator with two dimensions
-    // Example: M(1,1) returns row 1 and column 1 of matrix M.
-    const float& operator ()(int i, int j) const{
-      return (n[j][i]);
-    }
+  // Index operator with two dimensions
+  // Example: M(1,1) returns row 1 and column 1 of matrix M.
+  const float &operator ()(int i, int j) const {
+    return (n[j][i]);
+  }
 
-    // Return a single  vector from the matrix (row or columnn major? hmm).
-    Vector4f& operator [](int j){
-      return (*reinterpret_cast<Vector4f *>(n[j]));
-    }
+  // Return a single vector from the matrix.
+  Vector4f &operator [](int j) {
+    return (*reinterpret_cast<Vector4f *>(n[j]));
+  }
 
-    // Return a single  vector from the matrix (row or columnn major? hmm).
-    const Vector4f& operator [](int j) const{
-      return (*reinterpret_cast<const Vector4f *>(n[j]));
-    }
+  // Return a single vector from the matrix.
+  const Vector4f &operator [](int j) const {
+    return (*reinterpret_cast<const Vector4f *>(n[j]));
+  }
 
-    // Make a matrix rotate about various axis
-    Matrix4f MakeRotationX(float t){
-        // TODO:
-        return(Matrix4f()); // You will need to modify this.
-                            // When you test, test against glm_gtx_transform
-    }
-    Matrix4f MakeRotationY(float t){
-        // TODO:
-        return(Matrix4f()); // You will need to modify this.
-                            // When you test, test against glm_gtx_transform
-    }
-    Matrix4f MakeRotationZ(float t){
-        // TODO:
-        return(Matrix4f()); // You will need to modify this.
-                            // When you test, test against glm_gtx_transform
-    }
-    Matrix4f MakeScale(float sx,float sy, float sz){
-        // TODO:
-        return(Matrix4f()); // You will need to modify this.
-                            // When you test, test against glm_gtx_transform
-    }
-
-
+  // Builds a transformation matrix.
+  // TODO: Test against glm_gtx_transform
+  Matrix4f MakeRotationX(float t) {
+    return Matrix4f(
+      1, 0, 0, 0,
+      0, cos(t), -sin(t), 0,
+      0, sin(t), cos(t), 0,
+      0, 0, 0, 1
+    );
+  }
+  Matrix4f MakeRotationY(float t) {
+    return Matrix4f(
+      cos(t), 0, sin(t), 0,
+      0, 1, 0, 0,
+      -sin(t), 0, cos(t), 0,
+      0, 0, 0, 1
+    );
+  }
+  Matrix4f MakeRotationZ(float t) {
+    return Matrix4f(
+      cos(t), -sin(t), 0, 0,
+      sin(t), cos(t), 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    );
+  }
+  Matrix4f MakeScale(float sx, float sy, float sz) {
+    return Matrix4f(
+      sx, 0, 0, 0,
+      0, sy, 0, 0,
+      0, 0, sz, 0,
+      0, 0, 0, 1
+    );
+  }
 };
 
-// Matrix Multiplication
-Matrix4f operator *(const Matrix4f& A, const Matrix4f& B){
-  // TODO:
-  Matrix4f mat4;
-
-  return mat4;
+// Matrix multiplication
+Matrix4f operator *(const Matrix4f &A, const Matrix4f &B) {
+  return Matrix4f(
+    Dot(A[0], B[0]), Dot(A[0], B[1]), Dot(A[0], B[2]), Dot(A[0], B[3]),
+    Dot(A[1], B[0]), Dot(A[1], B[1]), Dot(A[1], B[2]), Dot(A[1], B[3]),
+    Dot(A[2], B[0]), Dot(A[2], B[1]), Dot(A[2], B[2]), Dot(A[2], B[3]),
+    Dot(A[3], B[0]), Dot(A[3], B[1]), Dot(A[3], B[2]), Dot(A[3], B[3])
+  );
 }
 
 // Matrix multiply by a vector
-
-Vector4f operator *(const Matrix4f& M, const Vector4f& v){
-  // TODO:
-  Vector4f vec;
-
-  return vec;
+Vector4f operator *(const Matrix4f &M, const Vector4f &v) {
+  return Vector4f(
+    Dot(M[0], v),
+    Dot(M[1], v),
+    Dot(M[2], v),
+    Dot(M[3], v)
+  );
 }
-
 
 #endif
--- Assignment1_MathFoundations1/include/Vector4f.h
+++ Assignment1_MathFoundations1/include/Vector4f.h
@@ -6,142 +6,160 @@
 // Vector4f performs vector operations with 4-dimensions
 // The purpose of this class is primarily for 3D graphics
 // applications.
-struct Vector4f{
-    // Note: x,y,z,w are a convention
-    // x,y,z,w could be position, but also any 4-component value.
-    float x,y,z,w;
+struct Vector4f {
+  // Note: x,y,z,w are a convention
+  // x,y,z,w could be position, but also any 4-component value.
+  float x, y, z, w;
 
-    // Default conostrutcor
-    // 'why default?' https://stackoverflow.com/questions/20828907/the-new-keyword-default-in-c11
-    Vector4f() = default;
+  // Default constructor
+  // 'why default?' https://stackoverflow.com/questions/20828907/the-new-keyword-default-in-c11
+  Vector4f() = default;
 
-    // The "Real" constructor we want to use.
-    // This initializes the values x,y,z
-    Vector4f(float a, float b, float c, float d){
-      // TODO:
-    }
+  // The "Real" constructor we want to use.
+  // This initializes the values x,y,z
+  Vector4f(float a, float b, float c, float d) {
+    x = a;
+    y = b;
+    z = c;
+    w = d;
+  }
 
-    // Index operator, allowing us to access the individual
-    // x,y,z,w components of our vector.
-    float& operator[](int i){
-        // TODO: Discuss with your partner why this works.
-        //       There is no code to change here.
-      return ((&x)[i]);
-    }
+  // Index operator, allowing us to access the individual
+  // x,y,z,w components of our vector.
+  float &operator[](int i) {
+    return ((&x)[i]);
+  }
 
-    // Index operator, allowing us to access the individual
-    // x,y,z,w components of our vector.
-    const float& operator[](int i) const{
-        // TODO: Discuss with your partner why this works.
-        //       There is no code to change here.
-        return ((&x)[i]);
-    }
+  // Index operator, allowing us to access the individual
+  // x,y,z,w components of our vector.
+  const float &operator[](int i) const {
+    return ((&x)[i]);
+  }
 
-    // Multiplication Operator
-    // Multiply vector by a uniform-scalar.
-    Vector4f& operator *=(float s){
-        // TODO:
-        return (*this);
-    }
+  // Multiplication Operator
+  // Multiply vector by a uniform-scalar.
+  Vector4f &operator *=(float s) {
+    x *= s;
+    y *= s;
+    z *= s;
+    w *= s;
+    return (*this);
+  }
 
-    // Division Operator
-    Vector4f& operator /=(float s){
-        // TODO:
+  // Division Operator
+  Vector4f &operator /=(float s) {
+    x /= s;
+    y /= s;
+    z /= s;
+    w /= s;
+    return (*this);
+  }
 
-        return (*this);
-    }
+  // Addition operator
+  Vector4f &operator +=(const Vector4f &v) {
+    x += v.x;
+    y += v.y;
+    z += v.z;
+    w += v.w;
+    return (*this);
+  }
 
-    // Addition operator
-    Vector4f& operator +=(const Vector4f& v){
-        // TODO:
-
-      return (*this);
-    }
-
-    // Subtraction operator
-    Vector4f& operator -=(const Vector4f& v){
-        // TODO:
-
-      return (*this);
-    }
-
+  // Subtraction operator
+  Vector4f &operator -=(const Vector4f &v) {
+    x -= v.x;
+    y -= v.y;
+    z -= v.z;
+    w -= v.w;
+    return (*this);
+  }
 };
 
 // Compute the dot product of a Vector4f
-inline float Dot(const Vector4f& a, const Vector4f& b){
-  // TODO:
-  return 0;
+inline float Dot(const Vector4f &a, const Vector4f &b) {
+  return (a.x * b.x) + (a.y * b.y) + (a.z * b.z) + (a.w * b.w);
 }
 
 // Multiplication of a vector by a scalar values
-inline Vector4f operator *(const Vector4f& v, float s){
-  // TODO:
-  Vector4f vec;
-  return vec;
+inline Vector4f operator *(const Vector4f &v, float s) {
+  return Vector4f(
+    v.x * s,
+    v.y * s,
+    v.z * s,
+    v.w * s
+  );
 }
 
 // Division of a vector by a scalar value.
-inline Vector4f operator /(const Vector4f& v, float s){
-  // TODO:
-  Vector4f vec;
-  return vec;
+inline Vector4f operator /(const Vector4f &v, float s) {
+  return Vector4f(
+    v.x / s,
+    v.y / s,
+    v.z / s,
+    v.w / s
+  );
 }
 
 // Negation of a vector
 // Use Case: Sometimes it is handy to apply a force in an opposite direction
-inline Vector4f operator -(const Vector4f& v){
-  // TODO:
-  Vector4f vec;
-  return vec;
+inline Vector4f operator -(const Vector4f &v) {
+  return Vector4f(
+    -v.x,
+    -v.y,
+    -v.z,
+    -v.w
+  );
 }
 
 // Return the magnitude of a vector
-inline float Magnitude(const Vector4f& v){
-  // TODO:
-  return 0;
+inline float Magnitude(const Vector4f &v) {
+  return sqrt((v.x * v.x) + (v.y * v.y) + (v.z * v.z) + (v.w * v.w));
 }
 
 // Add two vectors together
-inline Vector4f operator +(const Vector4f& a, const Vector4f& b){
-  // TODO:
-  Vector4f vec;
-  return vec;
+inline Vector4f operator +(const Vector4f &a, const Vector4f &b) {
+  return Vector4f(
+    a.x + b.x,
+    a.y + b.y,
+    a.z + b.z,
+    a.w + b.w
+  );
 }
 
 // Subtract two vectors
-inline Vector4f operator -(const Vector4f& a, const Vector4f& b){
-  // TODO:
-  Vector4f vec;
-  return vec;
+inline Vector4f operator -(const Vector4f &a, const Vector4f &b) {
+  return Vector4f(
+    a.x - b.x,
+    a.y - b.y,
+    a.z - b.z,
+    a.w - b.w
+  );
 }
 
 // Vector Projection
 // Note: This is the vector projection of 'a' onto 'b'
-inline Vector4f Project(const Vector4f& a, const Vector4f& b){
-  // TODO:
-  Vector4f vec;
-  return vec;
+inline Vector4f Project(const Vector4f &a, const Vector4f &b) {
+  float magB = Magnitude(b);
+  return b * (Dot(a, b) / (magB * magB));
 }
 
 // Set a vectors magnitude to 1
 // Note: This is NOT generating a normal vector
-inline Vector4f Normalize(const Vector4f& v){
-  // TODO:
-  Vector4f vec;
-  return vec;
+inline Vector4f Normalize(const Vector4f &v) {
+  return v / Magnitude(v);
 }
 
 // a x b (read: 'a crossed b')
 // Produces a new vector perpendicular to a and b.
 // (So long as a and b are not parallel which returns zero vector)
-// Note: For a Vector4f, we can only compute a cross porduct to 
+// Note: For a Vector4f, we can only compute a cross product to 
 //       to vectors in 3-dimensions. Simply ignore w, and set to (0,0,0,1)
 //       for this vector.
-inline Vector4f CrossProduct(const Vector4f& a, const Vector4f& b){
-  // TODO:
-  Vector4f vec;
-  return vec;
+inline Vector4f CrossProduct(const Vector4f &a, const Vector4f &b) {
+  return Vector4f(
+    (a.y * b.z) - (a.z * b.y),
+    (a.z * b.x) - (a.x * b.z),
+    (a.x * b.y) - (a.y * b.x),
+    1);
 }
-
 
 #endif
