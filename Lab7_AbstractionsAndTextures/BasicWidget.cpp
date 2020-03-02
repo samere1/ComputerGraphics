79baa7a4653e785854bfea8b2b5da947eca42307
selkheir <elkheir.s@husky.neu.edu>
2020-03-02 14:45:22 -0500

    [L7] Completed lab 7

--- Lab7_AbstractionsAndTextures/BasicWidget.cpp
+++ Lab7_AbstractionsAndTextures/BasicWidget.cpp
@@ -2,33 +2,35 @@
 
 //////////////////////////////////////////////////////////////////////
 // Publics
-BasicWidget::BasicWidget(QWidget* parent) : QOpenGLWidget(parent), logger_(this)
+BasicWidget::BasicWidget(QWidget *parent) : QOpenGLWidget(parent), logger_(this)
 {
   setFocusPolicy(Qt::StrongFocus);
 }
 
 BasicWidget::~BasicWidget()
 {
-    for (auto renderable : renderables_) {
-        delete renderable;
-    }
-    renderables_.clear();
+  for (auto renderable : renderables_) {
+    delete renderable;
+  }
+  renderables_.clear();
 }
 
 //////////////////////////////////////////////////////////////////////
 // Privates
 ///////////////////////////////////////////////////////////////////////
 // Protected
-void BasicWidget::keyReleaseEvent(QKeyEvent* keyEvent)
+void BasicWidget::keyReleaseEvent(QKeyEvent *keyEvent)
 {
   // Handle key events here.
   if (keyEvent->key() == Qt::Key_Left) {
     qDebug() << "Left Arrow Pressed";
     update();  // We call update after we handle a key press to trigger a redraw when we are ready
-  } else if (keyEvent->key() == Qt::Key_Right) {
+  }
+  else if (keyEvent->key() == Qt::Key_Right) {
     qDebug() << "Right Arrow Pressed";
     update();  // We call update after we handle a key press to trigger a redraw when we are ready
-  } else {
+  }
+  else {
     qDebug() << "You Pressed an unsupported Key!";
   }
 }
@@ -38,7 +40,7 @@ void BasicWidget::initializeGL()
   initializeOpenGLFunctions();
 
   qDebug() << QDir::currentPath();
-  QString texFile = "../../cat3.ppm";
+  QString texFile = "./cat3.ppm";
   QVector<QVector3D> pos;
   QVector<QVector3D> norm;
   QVector<QVector2D> texCoord;
@@ -52,36 +54,49 @@ void BasicWidget::initializeGL()
   norm << QVector3D(0.0, 0.0, 1.0);
   norm << QVector3D(0.0, 0.0, 1.0);
   norm << QVector3D(0.0, 0.0, 1.0);
-  // TODO:  Make sure to add texture coordinates to pass into the initialization of our renderable
+  // Add texture coordinates to pass into the initialization of our renderable
+  texCoord << QVector2D(0.0, 0.0);
+  texCoord << QVector2D(0.0, 1.0);
+  texCoord << QVector2D(1.0, 0.0);
+  texCoord << QVector2D(1.0, 1.0);
   idx << 0 << 1 << 2 << 2 << 1 << 3;
 
-  Renderable* ren = new Renderable();
+  Renderable *ren = new Renderable();
   ren->init(pos, norm, texCoord, idx, texFile);
+  ren->setRotationAxis(QVector3D(1, 1, 0));
   renderables_.push_back(ren);
+
+  Renderable *ren2 = new Renderable();
+  ren2->init(pos, norm, texCoord, idx, texFile);
+  ren2->setRotationAxis(QVector3D(1, -1, 0));
+  renderables_.push_back(ren2);
+
   glViewport(0, 0, width(), height());
   frameTimer_.start();
 }
 
 void BasicWidget::resizeGL(int w, int h)
 {
-    if (!logger_.isLogging()) {
-        logger_.initialize();
-        // Setup the logger for real-time messaging
-        connect(&logger_, &QOpenGLDebugLogger::messageLogged, [=]() {
-            const QList<QOpenGLDebugMessage> messages = logger_.loggedMessages();
-            for (auto msg : messages) {
-                qDebug() << msg;
-            }
-            });
-        logger_.startLogging();
-    }
-  glViewport(0, 0, w, h);
+  if (!logger_.isLogging()) {
+    logger_.initialize();
+    // Setup the logger for real-time messaging
+    connect(&logger_, &QOpenGLDebugLogger::messageLogged, [=]() {
+      const QList<QOpenGLDebugMessage> messages = logger_.loggedMessages();
+      for (auto msg : messages) {
+        qDebug() << msg;
+      }
+      });
+    logger_.startLogging();
+  }
+  
   view_.setToIdentity();
-  view_.lookAt(QVector3D(0.0f, 0.0f, 2.0f),
-      QVector3D(0.0f, 0.0f, 0.0f),
-      QVector3D(0.0f, 1.0f, 0.0f));
+  view_.lookAt(
+    QVector3D(0.0f, 0.0f, 2.0f),
+    QVector3D(0.0f, 0.0f, 0.0f),
+    QVector3D(0.0f, 1.0f, 0.0f));
   projection_.setToIdentity();
-  projection_.perspective(70.f, (float)w/(float)h, 0.001, 1000.0);
+  projection_.perspective(70.f, (float)w / (float)h, 0.001, 1000.0);
+
   glViewport(0, 0, w, h);
 }
 
@@ -95,8 +110,8 @@ void BasicWidget::paintGL()
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   for (auto renderable : renderables_) {
-      renderable->update(msSinceRestart);
-      renderable->draw(view_, projection_);
+    renderable->update(msSinceRestart);
+    renderable->draw(view_, projection_);
   }
   update();
 }
--- Lab7_AbstractionsAndTextures/frag.glsl
+++ Lab7_AbstractionsAndTextures/frag.glsl
@@ -5,9 +5,10 @@ in vec2 texCoords;
 // We always define a fragment color that we output.
 out vec4 fragColor;
 
-// TODO:  Add a sampler to retrieve our color data from!
+// Add a sampler to retrieve our color data from
+uniform sampler2D tex;
 
 void main() {
-  // Set our output fragment color to whatever we pull from our input texture (Note, change 'tex' to whatever the sampler is named)
+  // Set our output fragment color to whatever we pull from our input texture
   fragColor = texture(tex, texCoords);
 }
