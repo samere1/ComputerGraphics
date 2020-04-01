3e17607797ef65e534b56bd6a52ae8bf87ac0b2e
selkheir <elkheir.s@husky.neu.edu>
2020-04-01 18:44:25 -0400

    [L8] Add attenuation

--- Lab8_CameraAndIllumination/frag.glsl
+++ Lab8_CameraAndIllumination/frag.glsl
@@ -1,13 +1,11 @@
 #version 330
 
-// Take in our texture coordinate from our vertex shader
+// Inputs from vertex shader
 in vec2 texCoords;
-// And our normal
 in vec3 norm;
-// And our fragment position for lighting
 in vec3 fragPos;
 
-// We always define a fragment color that we output.
+// Fragment color that we output
 out vec4 fragColor;
 
 // Define our light(s)
@@ -26,25 +24,32 @@ struct PointLight {
     vec3 specular;
 };
 
-// Maintain our uniforms.
-uniform sampler2D tex;              // our primary texture
-uniform mat4 view;                  // we need the view matrix for highlights
-uniform PointLight pointLights[4];  // Our lights
+#define NUM_POINT_LIGHTS 4
+
+// Maintain our uniforms
+uniform sampler2D tex;              // Primary texture
+uniform PointLight pointLights[NUM_POINT_LIGHTS];  // Lights
 
 vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
-  // Compute ambient light
+  // Ambient light
   vec3 ambient = light.ambientIntensity * light.color;
   
-  // Compute diffuse light
+  // Diffuse light
   vec3 lightDir = normalize(light.position - fragPos);
   float diffImpact = max(dot(normal, lightDir), 0.0);
   vec3 diffuseLight = diffImpact * light.color;
   
-  // Compute specular lighting
-  vec3 reflectDir = reflect(-lightDir, norm);
-  
+  // Specular lighting
+  vec3 reflectDir = reflect(-lightDir, normal);
   float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
   vec3 specular = light.specularIntensity * spec * light.color;
+  
+  // Attenuation
+  float distance = length(light.position - fragPos);
+  float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+  ambient *= attenuation;
+  diffuseLight *= attenuation;
+  specular *= attenuation;
   
   return (ambient + diffuseLight + specular);
 }
@@ -60,7 +65,7 @@ void main() {
   
   // Calculate point lights
   vec3 lighting;
-  for (int i = 0; i < 4; ++i) {
+  for (int i = 0; i < NUM_POINT_LIGHTS; ++i) {
 	  lighting += CalcPointLight(pointLights[i], norm, fragPos, viewDir);
   }
   
